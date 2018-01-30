9b66653ee166ce720bfb5a7d75bba47a1c945d94
Michelle Livengood <ml2876@nyu.edu>
2018-01-30 18:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function logShout(string) {
   console.log("HELLO")
 }
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log("hello")
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase() )
-  return "I can't hear you!"
-  else if (string === string.toUpperCase() )
+  return "I can't hear you!";
+  else if (string === string.toUpperCase()
   return "YES INDEED!"
   else (string === "I love you, Grandma.")
   return "I love you, too."
